83f6d3ec97b2edf5d4c831858f46d8af0cc738b5
Brendan Lawlor <brendan.lawlor@gmail.com>
2021-02-23 21:00:09 +0100

    Additional testing and comments.

--- src/main/kotlin/ie/polymorphsoft/bitwig/Model.kt
+++ src/main/kotlin/ie/polymorphsoft/bitwig/Model.kt
@@ -1,4 +1,4 @@
-package ie.polymorphsoft
+package ie.polymorphsoft.bitwig
 
 import com.bitwig.extension.api.util.midi.ShortMidiMessage
 import ie.polymorphsoft.bitwig.zoom.InputActions
@@ -6,17 +6,28 @@ import ie.polymorphsoft.bitwig.zoom.InputEvent
 import ie.polymorphsoft.bitwig.zoom.Inputs
 import ie.polymorphsoft.bitwig.zoom.inputEvent
 
+/*
+This extension uses the Elm Architecture (https://guide.elm-lang.org/architecture/) widely adopted
+by other frameworks like React.
+
+The Model represents an immutable internal state of the extension which interacts with incoming InputEvents
+to produce new versions of the Model and optionally outgoing BitwigEvents (typically sent to the Bitbig API).
+
+The update function interprets the incoming events into model changes and events. Note that there is no
+knowledge of midi or the Bitwig API encoded in the model or the update function.
+ */
+
 enum class BitwigMode {
     ARRANGE, CLIP, MIX
 }
-
-
 
 sealed class BitwigEvent
 class SwitchToArrangeView() : BitwigEvent()
 class SwitchToClipLauncherView(): BitwigEvent()
 class SwitchToMixerView(): BitwigEvent()
 
+
+// The internal state of the extension is modeled here.
 data class Model(val mode: BitwigMode) {
     fun toggleMode(): Model {
         return when (mode) {
@@ -25,11 +36,27 @@ data class Model(val mode: BitwigMode) {
             BitwigMode.MIX -> this.copy(mode = BitwigMode.ARRANGE)
         }
     }
+
+    companion object {
+        fun initModel() = Model(BitwigMode.ARRANGE)
+        fun update(model: Model, inputEvent: InputEvent): Pair<Model, BitwigEvent?> {
+            return when(inputEvent){
+                InputEvent(Inputs.F5, InputActions.ON) -> {
+                    val newModel = model.toggleMode()
+                    return Pair(newModel, toggleViewEvent(newModel.mode))
+                }
+                else -> Pair(model, null)
+            }
+        }
+    }
 }
 
-fun controller(model: Model, midiMessage: ShortMidiMessage): Pair<Model, BitwigEvent?> {
-    return when(midiMessage.inputEvent()){
-        InputEvent(Inputs.F5, InputActions.ON) ->  Pair(model.toggleMode(),null)
-        else -> Pair(model, null)
+
+
+private fun toggleViewEvent(mode: BitwigMode): BitwigEvent {
+    return when (mode) {
+        BitwigMode.ARRANGE -> SwitchToArrangeView()
+        BitwigMode.CLIP -> SwitchToClipLauncherView()
+        BitwigMode.MIX -> SwitchToMixerView()
     }
 }
--- src/test/kotlin/ie/polymorphsoft/bitwig/ModelTest.kt
+++ src/test/kotlin/ie/polymorphsoft/bitwig/ModelTest.kt
@@ -1,17 +1,27 @@
 package ie.polymorphsoft.bitwig
 
-import com.bitwig.extension.api.util.midi.ShortMidiMessage
-import ie.polymorphsoft.BitwigMode
-import ie.polymorphsoft.Model
-import ie.polymorphsoft.controller
-import org.junit.jupiter.api.Assertions.*
+import ie.polymorphsoft.*
+import ie.polymorphsoft.bitwig.Model.Companion.update
+import ie.polymorphsoft.bitwig.zoom.InputActions
+import ie.polymorphsoft.bitwig.zoom.InputEvent
+import ie.polymorphsoft.bitwig.zoom.Inputs
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class ModelTest {
     @Test
     internal fun switchMode() {
         val model = Model(BitwigMode.ARRANGE)
-        val (newModel,event) = controller(model, ShortMidiMessage(144,58,127))
+        val inputEvent = InputEvent(Inputs.F5, InputActions.ON)
+        val (newModel, bitwigEvent) = update(model, inputEvent)
         assertEquals(BitwigMode.CLIP, newModel.mode)
+        assertTrue(bitwigEvent is SwitchToClipLauncherView)
+        val (newModel2, bitwigEvent2) = update(newModel, inputEvent)
+        assertEquals(BitwigMode.MIX, newModel2.mode)
+        assertTrue(bitwigEvent2 is SwitchToMixerView)
+        val (newModel3, bitwigEvent3) = update(newModel2, inputEvent)
+        assertEquals(BitwigMode.ARRANGE, newModel3.mode)
+        assertTrue(bitwigEvent3 is SwitchToArrangeView)
     }
 }
